d1e7821cf23b7325ce6e577b6237de0ce52ffc4d
thoweber <karazon@gmail.com>
2021-05-15 19:15:39 +0200

    Prepared jpeg optimizer module

--- README.md
+++ README.md
@@ -8,3 +8,14 @@
 
 ## Acknowledgements
 The Structural Similarity code is based on the work of https://github.com/rhys-e/structural-similarity.
+
+### Included Open Source software
+
+#### The JPEG optimizers module
+
+* Binary release of Mozilla JPEG Encoder Project in
+  [`optimizer-jpeg/src/main/resources/vendor/mozjpeg`](optimizer-jpeg/src/main/resources/vendor/mozjpeg)
+
+  License: [`optimizer-jpeg/src/main/resources/vendor/mozjpeg/LICENSE.md`](optimizer-jpeg/src/main/resources/vendor/mozjpeg/LICENSE.md)
+
+  More information at [github.com/mozilla/mozjpeg](https://github.com/mozilla/mozjpeg)
---
+++ optimizer-jpeg/src/main/java/guru/thomasweber/imgcmprss/optimizer/jpeg/MozJpegOptimizerUseCase.java
@@ -0,0 +1,29 @@
+package guru.thomasweber.imgcmprss.optimizer.jpeg;
+
+import guru.thomasweber.imgcmprss.model.ImageFormat;
+import guru.thomasweber.imgcmprss.model.ImageSource;
+import guru.thomasweber.imgcmprss.model.OptimizerResult;
+import guru.thomasweber.imgcmprss.model.OptimizerType;
+import guru.thomasweber.imgcmprss.usecase.OptimizerUseCase;
+
+public class MozJpegOptimizerUseCase implements OptimizerUseCase {
+	
+	public static final String VENDOR_PATH = "vendor/mozjpeg";
+
+	@Override
+	public ImageFormat getImageFormat() {
+		return ImageFormat.JPEG;
+	}
+
+	@Override
+	public OptimizerType getType() {
+		return OptimizerType.LOSSY;
+	}
+
+	@Override
+	public OptimizerResult optimize(ImageSource source) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
